3ed0616316289c80786fe9d103e1c568565e1220
kissrain <cjyang1123@gmail.com>
2016-07-23 23:25:28 +0800

    Initial Commit

---
+++ src/MyActionClass.java
@@ -0,0 +1,13 @@
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+/**
+ * Created by CJY on 16/7/23.
+ */
+public class MyActionClass extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        // TODO: insert action logic here
+    }
+}
